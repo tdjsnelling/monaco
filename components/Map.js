3c51db52e625de8cf0f28aff2841e83dd45ce990
Tom Snelling <tomsnelling8@gmail.com>
2023-03-24 10:58:58 +0000

    correct map rotation, add corner markers

--- components/Map.js
+++ components/Map.js
@@ -1,6 +1,26 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useMemo } from "react";
+import styled from "styled-components";
 
-const space = 400;
+const StyledMap = styled.div`
+  padding: var(--space-4);
+`;
+
+const space = 800;
+
+const rad = (deg) => deg * (Math.PI / 180);
+
+const rotate = (x, y, a, px, py) => {
+  const c = Math.cos(rad(a));
+  const s = Math.sin(rad(a));
+
+  x -= px;
+  y -= py;
+
+  const newX = x * c - y * s;
+  const newY = y * c + x * s;
+
+  return [newX + px, (newY + py) * -1];
+};
 
 const Map = ({ circuit, Position, DriverList, TimingData }) => {
   const [data, setData] = useState({});
@@ -24,30 +44,49 @@ const Map = ({ circuit, Position, DriverList, TimingData }) => {
 
   const hasData = !!Object.keys(data).length;
 
-  const minX = hasData ? Math.min(...data.x) - space : undefined;
-  const minY = hasData ? Math.min(...data.y) - space : undefined;
+  const rotatedPoints = useMemo(() => {
+    if (!hasData) return "";
+    return data.x.map((x, i) =>
+      rotate(
+        x,
+        data.y[i],
+        data.rotation,
+        (Math.max(...data.x) - Math.min(...data.x)) / 2,
+        (Math.max(...data.y) - Math.min(...data.y)) / 2
+      )
+    );
+  }, [hasData]);
+
+  const minX = hasData
+    ? Math.min(...rotatedPoints.map(([x]) => x)) - space
+    : undefined;
+  const minY = hasData
+    ? Math.min(...rotatedPoints.map(([, y]) => y)) - space
+    : undefined;
   const widthX = hasData
-    ? Math.max(...data.x) - Math.min(...data.x) + space * 2
+    ? Math.max(...rotatedPoints.map(([x]) => x)) - minX + space * 2
     : undefined;
   const widthY = hasData
-    ? Math.max(...data.y) - Math.min(...data.y) + space * 2
+    ? Math.max(...rotatedPoints.map(([, y]) => y)) - minY + space * 2
     : undefined;
 
   const stroke = (widthX + widthY) / 200;
 
   return hasData ? (
-    <div style={{ padding: "var(--space-4)" }}>
+    <StyledMap>
       <svg
         viewBox={`${minX} ${minY} ${widthX} ${widthY}`}
         width="100%"
-        height="600px"
+        height="500px"
       >
         <path
           stroke="var(--colour-fg)"
           strokeWidth={stroke}
           strokeLinejoin="round"
           fill="transparent"
-          d={`M0,0 ${data.x.map((x, i) => `L${x},${data.y[i]}`).join(" ")}`}
+          d={`M${rotatedPoints[0][0]},${rotatedPoints[0][1]} ${rotatedPoints
+            .map(([x, y]) => `L${x},${y}`)
+            .join(" ")}`}
         />
         {Object.entries(Position.Entries ?? {}).map(([racingNumber, pos]) => {
           const driver = DriverList[racingNumber];
@@ -57,18 +96,25 @@ const Map = ({ circuit, Position, DriverList, TimingData }) => {
             !timingData.KnockedOut &&
             !timingData.Retired &&
             !timingData.Stopped;
+          const [rx, ry] = rotate(
+            pos.X,
+            pos.Y,
+            data.rotation,
+            (Math.max(...data.x) - Math.min(...data.x)) / 2,
+            (Math.max(...data.y) - Math.min(...data.y)) / 2
+          );
           return (
             <g key={`pos-${racingNumber}`} opacity={onTrack ? 1 : 0.5}>
               <circle
-                cx={pos.X}
-                cy={pos.Y}
+                cx={rx}
+                cy={ry}
                 r={(stroke * 2) / (onTrack ? 1 : 2)}
                 fill={`#${driver.TeamColour}`}
                 style={{ transition: "200ms linear" }}
               />
               <text
-                x={pos.X + stroke * 2 + 100}
-                y={pos.Y + (stroke * 2) / 1.25}
+                x={rx + stroke * 2 + 50}
+                y={ry + (stroke * 2) / 1.25}
                 fill={`#${driver.TeamColour}`}
                 fontSize={stroke * 4}
                 style={{ transition: "200ms linear" }}
@@ -78,8 +124,55 @@ const Map = ({ circuit, Position, DriverList, TimingData }) => {
             </g>
           );
         })}
+        {data.corners.map((corner) => {
+          const [cornerX, cornerY] = rotate(
+            corner.trackPosition.x,
+            corner.trackPosition.y,
+            data.rotation,
+            (Math.max(...data.x) - Math.min(...data.x)) / 2,
+            (Math.max(...data.y) - Math.min(...data.y)) / 2
+          );
+
+          const [textX, textY] = rotate(
+            corner.trackPosition.x + stroke * 5 * Math.cos(rad(corner.angle)),
+            corner.trackPosition.y + stroke * 5 * Math.sin(rad(corner.angle)),
+            data.rotation,
+            (Math.max(...data.x) - Math.min(...data.x)) / 2,
+            (Math.max(...data.y) - Math.min(...data.y)) / 2
+          );
+
+          let string = `${corner.number}`;
+          if (corner.letter) string = string + corner.letter;
+
+          const fontSize = stroke * 2.5;
+
+          const lineX = textX + fontSize * (string.length * 0.25);
+          const lineY = textY - (textY > cornerY ? fontSize * 0.7 : 0);
+
+          return (
+            <g key={`corner-${corner.number}}`}>
+              <text
+                x={textX}
+                y={textY}
+                fontSize={fontSize}
+                fontWeight="bold"
+                fill="red"
+                stroke="var(--colour-bg)"
+                strokeWidth={fontSize / 40}
+              >
+                {string}
+              </text>
+              <path
+                stroke="red"
+                strokeWidth={stroke / 2}
+                opacity={0.25}
+                d={`M${cornerX},${cornerY} L${lineX},${lineY}`}
+              />
+            </g>
+          );
+        })}
       </svg>
-    </div>
+    </StyledMap>
   ) : null;
 };
 
